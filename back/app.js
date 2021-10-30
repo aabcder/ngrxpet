d3fe1798492d2583173aaddc462368eda5ac7e32
Anton Chichikov <tonychichikov@gmail.com>
2021-10-30 14:08:53 +0300

    user response added

--- back/app.js
+++ back/app.js
@@ -1,3 +1,5 @@
+const user = require("./controllers/user");
+
 const express = require("express");
 
 const BACKEND_PORT = 3000;
@@ -7,18 +9,20 @@ app.listen(BACKEND_PORT, () =>
   console.log(`app listening on port ${BACKEND_PORT};`)
 );
 
-app.get("/user", (req, res) => {
+app.get("/api/user", (req, res) => {
+  res.send(user);
+});
+
+app.get("/api/user-settings", (req, res) => {
   res.send("");
 });
 
-app.get("/user-settings", (req, res) => {
+app.get("/api/items-list", (req, res) => {
   res.send("");
 });
 
-app.get("/items-list", (req, res) => {
+app.get("/api/item", (req, res) => {
   res.send("");
 });
 
-app.get("/item", (req, res) => {
-  res.send("");
-});
+app.on("error", console.error);
