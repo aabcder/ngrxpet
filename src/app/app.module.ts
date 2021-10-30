9358b6d129aaac49c540007c0ff90c5a8067ad7d
Anton Chichikov <tonychichikov@gmail.com>
2021-10-30 14:47:07 +0300

    move components to separate folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { UserInfoComponent } from './user-info/user-info.component';
-import { UsersViewComponent } from './users-view/users-view.component';
-import { CompaniesViewComponent } from './companies-view/companies-view.component';
-import { CompanyInfoComponent } from './company-info/company-info.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { UsersViewComponent } from './components/users-view/users-view.component';
+import { CompaniesViewComponent } from './components/companies-view/companies-view.component';
+import { CompanyInfoComponent } from './components/company-info/company-info.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, SidebarComponent, UserInfoComponent, UsersViewComponent, CompaniesViewComponent, CompanyInfoComponent],
