d8bbf8d38687805d233780402f7a43f1e2fe1988
Anton Chichikov <tonychichikov@gmail.com>
2021-10-30 14:22:42 +0300

    components creted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { UsersViewComponent } from './users-view/users-view.component';
+import { CompaniesViewComponent } from './companies-view/companies-view.component';
+import { CompanyInfoComponent } from './company-info/company-info.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, SidebarComponent, UserInfoComponent, UsersViewComponent, CompaniesViewComponent, CompanyInfoComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
