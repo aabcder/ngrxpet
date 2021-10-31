2cec9fe130398f3d681224ae31b82558ce6e9d4e
Anton Chichikov <tonychichikov@gmail.com>
2021-10-31 19:22:15 +0200

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompaniesViewComponent } from './components/companies-view/companies-view.component';
+import { UsersViewComponent } from './components/users-view/users-view.component';
+import { UsersResolver } from './resolvers/users.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersViewComponent,
+    resolve: { users: UsersResolver },
+  },
+  {
+    path: 'companies',
+    component: CompaniesViewComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
