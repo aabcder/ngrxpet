21f6d7de76ea4b7a65f551f4f08a01f4991b3c4c
Anton Chichikov <tonychichikov@gmail.com>
2021-10-31 20:21:40 +0200

    users view

--- src/app/components/users-view/users-view.component.ts
+++ src/app/components/users-view/users-view.component.ts
@@ -4,6 +4,7 @@ import { User } from 'src/app/models/user';
 import { usersSelector } from 'src/app/store/selectors/users.selector';
 import { tap } from 'rxjs/operators';
 import { AppState } from 'src/app/appState/appState';
+import { changeUserPriority } from 'src/app/store/actions/usersList.action';
 
 @Component({
   selector: 'app-users-view',
@@ -21,5 +22,20 @@ export class UsersViewComponent {
         })
       )
       .subscribe();
+  }
+
+  changePriority(user: User, delta: number): void {
+    this.store.dispatch(changeUserPriority({ user: user, delta: delta }));
+  }
+
+  getColorByPriority(priority: number): string {
+    // if (priority === 0) return '';
+    if (priority < -20) return 'p-3';
+    if (priority < -10) return 'p-2';
+    if (priority < -3) return 'p-1';
+    if (priority > 20) return 'p3';
+    if (priority > 10) return 'p2';
+    if (priority > 3) return 'p1';
+    return 'p0';
   }
 }
