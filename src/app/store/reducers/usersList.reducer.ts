0b0c8e1d99067bdc8ff34ed476fdd0cfa0791241
Anton Chichikov <tonychichikov@gmail.com>
2021-10-31 20:18:26 +0200

    priority change reducer

--- src/app/store/reducers/usersList.reducer.ts
+++ src/app/store/reducers/usersList.reducer.ts
@@ -1,12 +1,24 @@
 import { createReducer, on } from '@ngrx/store';
-import { setUsersList } from '../actions/usersList.action';
+import { changeUserPriority, setUsersList } from '../actions/usersList.action';
 import { User } from 'src/app/models/user';
 
 const initial: User[] = [];
 
 export const usersListReducer = createReducer(
   initial,
-  on(setUsersList, (state: User[], users: { users: User[]}) => {
-    return users?.users
+  on(setUsersList, (state: User[], users: { users: User[] }) => {
+    return users?.users;
+  }),
+  on(changeUserPriority, (state, action: { user: User; delta: number }) => {
+    return state
+      .map((current) => {
+        let fresh = Object.assign({}, current);
+        if (fresh.id === action.user.id)
+          fresh.priority = fresh.priority + action.delta;
+        return fresh;
+      })
+      .sort((a, b) =>
+        a.priority === b.priority ? 0 : a.priority > b.priority ? -1 : 1
+      );
   })
 );
