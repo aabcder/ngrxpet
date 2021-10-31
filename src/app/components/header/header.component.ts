4abb26e382d58ac8d470cc39e94289a138556069
Anton Chichikov <tonychichikov@gmail.com>
2021-10-31 17:05:23 +0200

    header improvements

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/appState/appState';
+import { User } from 'src/app/models/user';
+import {toggleSidebar} from 'src/app/store/actions/sidebar.actions';
+import { userSelector } from 'src/app/store/selectors/user.selectors';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HeaderComponent {
+  user: Observable<User>;
+  constructor(private store: Store<AppState>) {
+    this.user = store.select(userSelector);
   }
 
+  toggleSidebar(): void {
+    this.store.dispatch(toggleSidebar())
+  }
 }
