a08c436476c2ac51d7203d5d41ccbc74deb2d6a8
Anton Chichikov <tonychichikov@gmail.com>
2021-10-31 19:21:37 +0200

    users view data added

--- src/app/components/users-view/users-view.component.ts
+++ src/app/components/users-view/users-view.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { User } from 'src/app/models/user';
+import { usersSelector } from 'src/app/store/selectors/users.selector';
+import { tap } from 'rxjs/operators';
+import { AppState } from 'src/app/appState/appState';
 
 @Component({
   selector: 'app-users-view',
   templateUrl: './users-view.component.html',
-  styleUrls: ['./users-view.component.scss']
+  styleUrls: ['./users-view.component.scss'],
 })
-export class UsersViewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class UsersViewComponent {
+  users: User[] = [];
+  constructor(private store: Store<AppState>) {
+    store
+      .select(usersSelector)
+      .pipe(
+        tap((users) => {
+          this.users = users;
+        })
+      )
+      .subscribe();
   }
-
 }
