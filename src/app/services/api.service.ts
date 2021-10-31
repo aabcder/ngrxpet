45a45878764a193f5f6ea690a5612c3f6944de5a
Anton Chichikov <tonychichikov@gmail.com>
2021-10-31 08:59:19 +0200

    api service created

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { first, tap } from 'rxjs/operators';
+import { setUser } from '../store/actions/user.actions';
+import { User } from '../models/user';
+import { AppState } from 'src/app/appState/appState';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ApiService {}
+export class ApiService {
+  constructor(private http: HttpClient, private store: Store<AppState>) {}
+
+  getUserData(): void {
+    this.http
+      .get('/api/user')
+      .pipe(first(), tap(console.warn))
+      .subscribe((user: User) => {
+        this.store.dispatch(setUser({ user }));
+      });
+  }
+}
