eb9739b206d7ced0632c91e1b817312c574ba615
Anton Chichikov <tonychichikov@gmail.com>
2021-10-31 09:05:40 +0200

    user info connected to steam

--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { AppState } from 'src/app/appState/appState';
+import { userSelector } from 'src/app/store/selectors/user.selectors';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
-  styleUrls: ['./user-info.component.sass']
+  styleUrls: ['./user-info.component.sass'],
 })
-export class UserInfoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class UserInfoComponent {
+  user: Observable<any> = of(null);
+  constructor(private store: Store<AppState>) {
+    this.user = store.select(userSelector);
   }
-
 }
